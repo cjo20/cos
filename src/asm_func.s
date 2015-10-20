933ba729067a67a6c8182350e963e80e83501276
Chris Oattes <git@cjo20.net>
2015-10-20 13:34:42 +0100

    Minor reordering of code

--- src/asm_func.s
+++ src/asm_func.s
@@ -7,16 +7,17 @@ global flush_gdt
 ;stack: [esp + 4] Pointer to gdt
 ;		[esp	] The return address
 flush_gdt:
+
 	mov eax, [esp + 4]
 	lgdt 	[eax]	; read a byte from the I/O port and store it in the al register
+	jmp 0x08:flush2
+flush2:
 	mov ax, 0x10
 	mov dx, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
 	mov ss, ax
-	jmp 0x08:flush2
-flush2:
 	ret 				; return
 
 
@@ -24,3 +25,4 @@ global io_wait
 
 io_wait:
 	ret
+
