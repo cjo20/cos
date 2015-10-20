bc29164f820674d0619020b3a21cb8299d022beb
Chris Oattes <git@cjo20.net>
2015-10-20 03:25:21 +0100

    Add lib

---
+++ headers/lib.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDE_LIB_H
+#define INCLUDE_LIB_H
+int strlen(char * s);
+void strrev(char *begin, char * end);
+void itoa(int value, char * buffer, int radix);
+#endif
---
+++ src/lib.c
@@ -0,0 +1,64 @@
+#include "lib.h"
+
+
+void strrev(char *begin, char * end)
+{
+	char aux;
+
+	while (end > begin)
+	{
+		aux = *end;
+		*end = *begin;
+		end--;
+
+		*begin = aux;
+		begin++;
+	}
+}
+
+void itoa(int value, char * str, int base)
+{
+	static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+	char * wstr = str;
+
+	int sign;
+
+	if (base < 2 || base > 35)
+	{
+		*wstr = '\0';
+		return;
+	}
+
+	if ((sign=value) < 0)
+	{
+		value = -value;
+	}	
+
+	do 
+	{
+		*wstr++ = num[value % base];
+	} while (value /= base);
+
+	if (sign < 0)
+	{
+		*wstr++ = '-';
+	}
+
+	*wstr='\0';
+
+	strrev(str, wstr-1);
+}
+
+
+int strlen(char * s)
+{
+	int i = 0;
+
+	while(*s != 0)
+	{
+		i++;
+		s++;
+	}
+
+	return i;
+}
