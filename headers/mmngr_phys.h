38eb4bb4b17b2a1c9bb5211acd6e85ce4e1e14fc
Chris Oattes <git@cjo20.net>
2015-10-22 21:32:30 +0100

    Adding physical memory manager

---
+++ headers/mmngr_phys.h
@@ -0,0 +1,24 @@
+#ifndef HEADER_MMNGR_PHYS_H
+#define HEADER_MMNGR_PHYS_H
+
+#include "stdint.h"
+
+#define PMMNGR_BLOCKS_PER_BYTE 	8
+#define PMMNGR_BLOCK_SIZE 		4096
+#define PMMNGR_BLOCK_ALIGN		PMMNGR_BLOCK_SIZE
+
+
+
+typedef uint32_t physical_addr;
+
+inline void mmap_set (int bit);
+inline void mmap_unset (int bit);
+inline char mmap_test (int bit);
+
+void pmmngr_init(size_t mem_size, uint32_t * bitmap);
+void pmmngr_init_region(physical_addr base, size_t size);
+void pmmngr_deinit_region(physical_addr base, size_t size);
+void * pmmngr_alloc_block();
+void pmmngr_free_block(void * p);
+
+#endif
