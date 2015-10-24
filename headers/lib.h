725f1d3439851be461e883a4ff5b151a95dbe541
Chris Oattes <git@cjo20.net>
2015-10-24 05:59:22 +0100

    strtoul, isalpha, isupper, isdigit

--- headers/lib.h
+++ headers/lib.h
@@ -3,6 +3,7 @@
 int strlen(char * s);
 void strrev(char *begin, char * end);
 int strcmp(const char * s1, const char * s2);
+unsigned long strtoul(const char * str, char ** endptr, int base);
 void itoa(int value, char * buffer, int radix);
 void utoa(unsigned int value, char * buffer, int radix);
 int atoi(char * str);
--- src/lib.c
+++ src/lib.c
@@ -1,6 +1,6 @@
 #include "lib.h"
-
-
+#include "stdint.h"
+#include "fb.h"
 void strrev(char *begin, char * end)
 {
 	char aux;
@@ -30,6 +30,137 @@ int strcmp(const char * s1, const char * s2)
 	c2 = *((unsigned char *) s2);
 
 	return c1 - c2;
+}
+
+inline int isspace(int c)
+{
+	return (c == ' '
+		 || c == '\t'
+		 || c == '\n'
+		 || c == '\12');
+}
+
+inline int isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+		 
+}
+
+inline int isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') 
+		  || (c >= 'A' && c <= 'Z'));
+}
+
+inline int isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
+unsigned long strtoul(const char * str, char ** endptr, int base)
+{
+	unsigned char c;
+
+	const char * inp = str;
+	do 
+	{
+		c = *inp++;
+	} while(isspace(c));
+	
+	if ((base == 0 || base == 2)
+		&& c == '0' 
+		&& (*inp == 'b' || *inp == 'B'))
+	{
+		inp++; //skip x
+		c = *inp++;
+		base = 2;
+	}
+	else if ((base == 0 || base == 16)
+		&& c == '0' 
+		&& (*inp == 'x' || *inp == 'X'))
+	{
+		inp++; //skip x
+		c = *inp++;
+		base = 16;
+	}
+	else if (base == 0)
+	{
+		if (c == '0')
+		{
+			base = 8;
+		}
+		else
+		{
+			base = 10;
+		}
+	}
+	
+	unsigned long largestValue = 0;
+	unsigned long cutlim = 0;
+	unsigned long cutoff = 0;
+	unsigned long result = 0;
+	char any;
+
+
+	largestValue = 0xFFFFFFFF;
+	cutlim = largestValue % (unsigned long) base;
+	cutoff = largestValue / (unsigned long) base;
+
+	for (result = 0, any = 0; ; c = *inp++)
+	{
+		if (isdigit(c))
+		{
+			c -= '0';
+		}
+		else if (isalpha(c))
+		{
+			if (isupper(c))
+			{
+				c -= 'A';
+				c += 10;
+			}
+			else
+			{
+				c -= 'a';
+				c += 10;
+			}
+		}
+		else
+		{
+			break;
+		}
+
+		if (c >= base)
+		{
+			break;
+		}
+
+		if (any < 0 
+		 || result > cutoff 
+		 || (result == cutoff && c > cutlim))
+		{
+			any = -1;
+		}
+		else
+		{
+			any = 1;
+			result *= base;
+			result += c;
+		}
+	}
+
+	if (any < 0)
+	{
+			result = 0xFFFFFFFF;
+	}
+
+	if (endptr != 0)
+	{
+		*endptr = (char *) (any ? inp - 1 : str);
+	}
+
+	return result;
+
 }
 
 int atoi(char * str)
